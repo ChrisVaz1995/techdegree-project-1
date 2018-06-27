32627b6fc438836a0d2148f6e53ae7817739522a
Christopher Vazquez <christopher.vazquez2015@gmail.com>
2018-06-26 19:51:14 -0500

    Updated through Atom now.
    I was using brackets, and it kept giving me error codes. So I decided to use Atom instead, and everything makes it clearer. Brackets was just giving me false errors.

--- js/script.js
+++ js/script.js
@@ -1,40 +1,40 @@
 var quotes = [
-    
+
     {
         quote: "Not everyone who works hard is rewarded. But! All those who succeed have worked hard!",
         source: 'Kamogawa Genji',
         citation: 'Hajime no Ippo: New Challenger',
         year: 2009
     },
-    
+
     {
-    
+
     quote: "I've loved, I've hated, I've cried, I've died inside. I've resurrected but don't neglect that I've been modified. I'm stronger, faster, better than ever before.",
     source: 'Logic',
     citation: 'Welcome to Forever album',
     year: 2013
     },
-    
+
     {
-    
+
     quote: "God tested me to find the best of me, and I know I'm going to make it even without a degree.",
     source: 'Logic',
     citation: 'Young Sinatra album',
     year: 2011
     },
-    
+
     {
-    
+
     quote: "Our greatest glory is not in never falling, but in rising every time we fall.",
     source: 'Confucius'
     },
-    
+
     {
-    
+
     quote: "The hardest walk you can make is the walk you make alone. But that is the walk that makes you the strongest.",
     source: 'Unknown'
     }
-    
+
 ];
 
 
@@ -42,37 +42,32 @@ var quotes = [
 
 
 var randomQuote = ' ';
-var randomNumber;
-var displayMessage;
-var quote;
+var message;
+
 
 function getRandomQuote() {
-    
+
     var randomNumber = Math.floor(Math.random() * quotes.length );
-     var randomQuote = quotes[randomNumber];
     return randomQuote;
-    
+
 }
 
 // call the getRandomQuote here and call the items in the array to put it in a message down below. .
-function printQuote() {
-    var message = getRandomQuote();
-    var quote = displayMessage.quote;
-    var source = displayMessage.source;
-    var citation = displayMessage.citation;
-    var year = displayMessage.year;
-    
-    message + "<p class='quote'"> + quote + "</p>" + "<p class='source'"> + source + '.';
-    message + "<span class='citation'"> + citation + "</span>" + "<span class='year'"> + year + "</span>";
 
+function printQuote() {
+    var currentQuote = getRandomQuote();
+    var message = "<p class='quote'>" + currentQuote.quote + "</p>";
+    message += "<p class='quote'>" + currentQuote.source + "</p>" + "<p class='source'>" + '.';
+    message += "<span class='citation'>" + currentQuote.citation + "</span>"
+    message += "<span class'year'>" + currentQuote.year + "</span>"
 }
 
+    if (currentQuote.year !== undefined) {
+        message += "<span class='year'>" + currentQuote.year + "</span>";
+    } else {
+        message.getElementById('year');
+    }
 
-
-
-
-  document.getElementById("quote-box").innerHTML = displayMessage;
-  document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-
-
+document.getElementById("quote-box").innerHTML = message; {
+}
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
